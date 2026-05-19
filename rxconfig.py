5b388cac489655cb0a1f9ffa8b8d7ce8de8a6e06
jhparente <parentejohnrel98@gmail.com>
2026-05-19 20:38:25 +0800

    chore: remove static port and API URL configurations from rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,9 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Expense_Tracker_Frontend",
-    api_url="http://127.0.0.1:8001",
-    backend_port=8001,
-    frontend_port=3000,
+    # Ports and API URL are dynamically configured for single-port deployment
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
